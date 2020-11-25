d36b40ed6ce79fa7f52d8a28241601a61b36d9b0
Denis Voloshin <ihostility@gmail.com>
2020-11-25 14:28:59 +0300

    Initial commit

---
+++ src/main/java/com/keycloak/springkey/SpringkeyApplication.java
@@ -0,0 +1,13 @@
+package com.keycloak.springkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keycloak/springkey/SpringkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak.springkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
