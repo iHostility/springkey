b4bc4b22458310dc0327d628c11343467867c581
Denis Voloshin <ihostility@gmail.com>
2020-11-25 18:17:01 +0300

    clean up

--- src/main/java/com/keycloak/springkey/controllers/HelloController.java
+++ src/main/java/com/keycloak/springkey/controllers/HelloController.java
@@ -1,19 +1,18 @@
 package com.keycloak.springkey.controllers;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "welcome";
+    }
 
     @GetMapping("/hello")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        return "welcome";
+    public String hello() {
+        return "hello";
     }
 
 }
